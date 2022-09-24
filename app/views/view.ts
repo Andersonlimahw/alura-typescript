c51bed0a5e9af7ebe029e9da939bc774e250662a
Anderson Lima <andersonlimahw@gmail.com>
2022-09-23 22:19:29 -0300

    🆕 [O.O] - abstract class

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,13 +1,11 @@
-export class View<T> {
+export abstract class View<T> {
     protected elemento: HTMLElement;
 
     constructor(seletor : string) {
         this.elemento = document.querySelector(seletor);
     }
 
-    template(model : T): string {
-        throw Error('A classe filha deve implementar o método template()');
-    }
+    abstract template(model : T): string;
 
     update(model : T) : void {
         const template  = this.template(model);
--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -2,9 +2,6 @@ export class View {
     constructor(seletor) {
         this.elemento = document.querySelector(seletor);
     }
-    template(model) {
-        throw Error('A classe filha deve implementar o método template()');
-    }
     update(model) {
         const template = this.template(model);
         this.elemento.innerHTML = template;
